335134af86597e9ad9e353ac7076102eecbaf328
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2025-06-15 08:50:38 +0200

    HM-27 // Initialized Terraform to create resources

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source = "./modules/VPC"
+}
+
+resource "aws_db_subnet_group" "library" {
+  name       = "library-db-subnet-group"
+  subnet_ids = [
+    module.vpc.public_subnet1_id,
+    module.vpc.public_subnet2_id
+  ]
+
+  tags = {
+    Name = "Library DB subnet group"
+  }
+}
+
+resource "aws_security_group" "library-db-security-group" {
+  name   = "library-db-security-group"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [var.local_ip]
+  }
+
+  tags = {
+    Name = "library-db-security-group"
+  }
+}
+
+resource "aws_db_instance" "library-db" {
+  identifier               = "library-db"
+  engine                   = "mysql"
+  engine_version           = "8.0"
+  instance_class           = "db.t3.micro"
+  allocated_storage        = 20
+  db_name                  = var.db_name
+  username                 = var.root_user
+  password                 = var.root_password
+  publicly_accessible      = true
+  skip_final_snapshot      = true
+  storage_type             = "gp2"
+  vpc_security_group_ids   = [aws_security_group.library-db-security-group.id]
+  db_subnet_group_name     = aws_db_subnet_group.library.name
+
+  tags = {
+    Name = "library-db"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {}
+variable "local_ip" {}
+variable "db_name" {}
+variable "root_user" {}
+variable "root_password" {}
